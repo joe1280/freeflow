e15968f23a0db808729657651528ea869c50c46a
joe1280 <4334838@qq.com>
2016-07-26 22:56:14 +0800

    this is dog file
    this is dog file

---
+++ clear.bat
@@ -0,0 +1,8 @@
+del /q C:\WINDOWS\system32\LogFiles\HTTPERR\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC81\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC253129320\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC1447250484\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC1533658200\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC741696375\*.*
+del /q C:\WINDOWS\system32\LogFiles\W3SVC11\*.*
+
