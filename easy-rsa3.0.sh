569306c5badbbac953b63c2312de49419f8ea978
joe1280 <4334838@qq.com>
2016-10-10 13:32:12 +0800

    Update easy-rsa3.0.sh

--- easy-rsa3.0.sh
+++ easy-rsa3.0.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 wget https://github.com/joe1280/freeflow/raw/master/easy-rsa3.0.zip
-yum install unzip lrzsz
+yum -y install unzip lrzsz
 unzip easy-rsa3.0.zip
 chmod 755 login.sh logout.sh
 
