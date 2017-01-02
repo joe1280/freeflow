59c8d8afc85fad5633df6f9f8923c1df30a7d588
joe1280 <4334838@qq.com>
2017-01-02 20:53:03 +0800

    Update kong53.sh

--- kong53.sh
+++ kong53.sh
@@ -4,3 +4,6 @@ git clone https://github.com/examplecode/mproxy.git
 cd mproxy
 gcc -o mproxy mproxy.c
 ./mproxy -l 53 -d
+iptables -A INPUT -p tcp --dport  53 -j ACCEPT
+service iptables save
+service iptables restart
